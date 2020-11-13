9930fd942d2fd4e2e0bc285f12e66685bbd09bb0
Ellen Sparrow <EllenSparrow@MacBook-Pro-Ellen.local>
2020-11-13 17:31:12 +0300

    Updates

---
+++ precache-manifest.e5b063ab2145d934378a53f84d0040cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604f7d08c2930e49afd1516a4cc4b347",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d115b9edd70ce9acda8",
+    "url": "./static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "6858b48f485dba872685",
+    "url": "./static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "0d115b9edd70ce9acda8",
+    "url": "./static/js/2.c84a817c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c84a817c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6858b48f485dba872685",
+    "url": "./static/js/main.f208e5f8.chunk.js"
+  },
+  {
+    "revision": "b793c38fc953fa14b65d",
+    "url": "./static/js/runtime-main.208b55f4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mini-react-app/precache-manifest.944ab93bc739fe512006f27e28077190.js"
+  "./precache-manifest.e5b063ab2145d934378a53f84d0040cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mini-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
